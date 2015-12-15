95900491b936dd34373671cfc1eac7df1e0f64a3
thaalum <1151181@isep.ipp.pt>
2015-12-15 18:55:09 +0000

    .

---
+++ src/simplex/Simplex.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplex;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author fabiopinto
+ */
+public class Simplex {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        lerFicheiro(ler);
+    }
+    
+    
+    public static String[] lerFicheiro(String ficheiro) throws FileNotFoundException {
+        Scanner ler = new Scanner(new File(ficheiro));
+        int numeroLinhas = 0;
+        while (ler.hasNextLine() && numeroLinhas <= 230) {
+            String aux = ler.nextLine();
+            if (!aux.isEmpty()) {
+                numeroLinhas++;
+            }
+        }
+        
+
+    }
+}
