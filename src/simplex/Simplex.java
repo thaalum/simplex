7e2274a035810e61c6ddf91861c30e4180ca1332
thaalum <1151181@isep.ipp.pt>
2015-12-15 22:31:26 +0000

    retornaNumLinhasFicheiro e testeRetornaNumLinhasFicheiro

--- src/simplex/Simplex.java
+++ src/simplex/Simplex.java
@@ -5,13 +5,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Simplex {
-    public static void main() throws FileNotFoundException{
+
+    public static void main(String ficheiroInput, String ficheiroOutput) throws FileNotFoundException {
         System.out.println("Insira o nome do ficheiro: ");
         Scanner ler = new Scanner(System.in);
-        int numLinhas=retornaNumLinhasFicheiro(ler.toString());
+        int numLinhas = retornaNumLinhasFicheiro(ler.toString());
         System.out.println(numLinhas);
+        testesUnitarios.testeRetornaNumLinhasFicheiro();
     }
-    
+
+    /**
+     *
+     * @param ficheiro
+     * @return
+     * @throws FileNotFoundException
+     */
     public static int retornaNumLinhasFicheiro(String ficheiro) throws FileNotFoundException {
         Scanner ler = new Scanner(new File(ficheiro));
         int numeroLinhas = 0;
@@ -23,7 +31,5 @@ public class Simplex {
         }
         return numeroLinhas;
     }
-    
-    
-    
+
 }
---
+++ src/simplex/testesUnitarios.java
@@ -0,0 +1,33 @@
+
+package simplex;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class testesUnitarios {
+    
+    public static void testeRetornaNumLinhasFicheiro() throws FileNotFoundException {
+        File ficheiro1 = new File("testeRetornaNumLinhasFicheiro1");
+        File ficheiro2 = new File("testeRetornaNumLinhasFicheiro2");
+        int numeroLinhas = Simplex.retornaNumLinhasFicheiro(ficheiro1.toString());
+        if (numeroLinhas == 6)
+        {
+            System.out.println("1 - testeReturnaNumLinhasFicheiro1");
+        }
+        else
+        {
+            System.out.println("0 - testeReturnaNumLinhasFicheiro1");
+        }
+        numeroLinhas = Simplex.retornaNumLinhasFicheiro(ficheiro2.toString());
+        if (numeroLinhas == 5)
+        {
+            System.out.println("1 - testeReturnaNumLinhasFicheiro2");
+        }
+        else
+        {
+            System.out.println("0 - testeReturnaNumLinhasFicheiro2");
+        }
+        
+    }
+    
+}
