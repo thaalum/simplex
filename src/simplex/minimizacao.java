3a788df3c6eba8cf379153b4350e8e1550ffa812
CarlosTeix <teixeira.carlos@gmail.com>
2016-01-13 23:27:37 +0000

    transposta criada na class minimização

---
+++ src/simplex/minimizacao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplex;
+
+/**
+ *
+ * @author teixe
+ */
+public class minimizacao {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double [][] transposta (double [][]matriz) {
+        double transposta[][] = new double[matriz[0].length][matriz.length];
+        for (int lin = 0; lin < matriz.length; lin++) {
+            for (int col = 0; col < matriz[lin].length; col++) {
+                transposta[col][lin] = matriz[lin][col];
+            }
+        }
+        return transposta;
+    }
+}
